e43362e2894fa936a16960e3d4254f6ee5675e2b
Samuelpessoac <166840025+Samuelpessoac@users.noreply.github.com>
2024-05-10 11:07:36 -0300

    Update MainActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/MainActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/MainActivity.kt
@@ -11,22 +11,26 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Infla o layout da tela principal
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-
+// Obtém o nome de usuário da intent
         val username = intent.extras?.getString("username")
 
+        // Se o nome de usuário não estiver vazio, exibe uma saudação personalizada
         if(!username.isNullOrEmpty()){
             binding.textOla.setText("Olá $username")
         }
 
+        // Configura o botão "Fechar" para finalizar a atividade
         binding.buttonFechar.setOnClickListener {
             finishAffinity()
         }
 
+        // Configura o botão "Pedir" para iniciar a SplashActivity com as quantidades informadas
         binding.buttonPedir.setOnClickListener {
             val i = Intent(this, SplashActivity::class.java)
             i.putExtra("quantidadePizza", binding.editQuantidadePizza.text.toString())
@@ -37,31 +41,40 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        // Configura o CheckBox para a pizza
         binding.checkPizza.setOnClickListener {
             if(binding.checkPizza.isChecked()){
+                // Se o CheckBox estiver marcado, define a quantidade de pizza como 1 e torna o preço visível
                 binding.editQuantidadePizza.setText("1")
                 binding.textPrecoPizza.visibility = View.VISIBLE
             }else{
+                 // Caso contrário, define a quantidade de pizza como 0 e oculta o preço
                 binding.editQuantidadePizza.setText("0")
                 binding.textPrecoPizza.visibility = View.GONE
             }
         }
 
+        // Configura o CheckBox para a salada
         binding.checkSalada.setOnClickListener {
             if(binding.checkSalada.isChecked()){
+                 // Se o CheckBox estiver marcado, define a quantidade de salada como 1 e torna o preço visível
                 binding.editQuantidadeSalada.setText("1")
                 binding.textPrecoSalada.visibility = View.VISIBLE
             }else{
+                // Caso contrário, define a quantidade de salada como 0 e oculta o preço
                 binding.editQuantidadeSalada.setText("0")
                 binding.textPrecoSalada.visibility = View.GONE
             }
         }
 
+        // Configura o CheckBox para o hambúrguer
         binding.checkHamburger.setOnClickListener {
             if(binding.checkHamburger.isChecked()){
+                // Se o CheckBox estiver marcado, define a quantidade de hambúrguer como 1 e torna o preço visível
                 binding.editQuantidadeHamburguer.setText("1")
                 binding.textPrecoHamburguer.visibility = View.VISIBLE
             }else{
+                // Caso contrário, define a quantidade de hambúrguer como 0 e oculta o preço
                 binding.editQuantidadeHamburguer.setText("0")
                 binding.textPrecoHamburguer.visibility = View.GONE
             }
