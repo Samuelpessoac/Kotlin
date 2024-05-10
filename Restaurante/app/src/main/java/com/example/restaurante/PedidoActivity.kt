8cdd6558a41ae07869ea3744b5af71a15c7be84a
Samuelpessoac <166840025+Samuelpessoac@users.noreply.github.com>
2024-05-10 11:10:54 -0300

    Update PedidoActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/PedidoActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/PedidoActivity.kt
@@ -7,20 +7,24 @@ import com.example.restaurante.databinding.ActivityPedidoBinding
 class PedidoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPedidoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Infla o layout da tela de pedido
         binding = ActivityPedidoBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        // Obtém os valores das quantidades de pizza, salada e hambúrguer da intent
         val i = intent
         val quantidadePizza = i.getStringExtra("quantidadePizza").toString().toInt()
         val quantidadeSalada = i.getStringExtra("quantidadeSalada").toString().toInt()
         val quantidadeHamburguer = i.getStringExtra("quantidadeHamburguer").toString().toInt()
 
+         // Cria o texto do resumo do pedido
         val texto = "Resumo do Pedido\n" +
                 "Pizza: $quantidadePizza Preço: ${quantidadePizza*8}\n" +
                 "Salada: $quantidadeSalada Preço: ${quantidadeSalada*10}\n" +
                 "Hamburguer: $quantidadeHamburguer Preço: ${quantidadeHamburguer*12}\n"
 
+        // Exibe o resumo na tela
         binding.textResumo.text = texto
 
     }
